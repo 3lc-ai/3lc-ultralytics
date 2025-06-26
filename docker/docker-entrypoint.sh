1f70ffea45d7f044541e37bd753d3505e6d5c8a7
Jørgen Lind <jorgen@3lc.ai>
2025-06-26 13:17:38 +0200

    random tracker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,13 +3,11 @@ set -e
 
 source /app/.venv/bin/activate
 cd /app/3lc-ultralytics
-# Install "live" dependencies
-uv sync --no-sources --active
 
 # If no command is provided, run the tests
 if [ $# -eq 0 ]; then
     echo "Running tests..."
-    uv run --no-sources --active pytest -k "test_dataset_determinism[train]"
+    uv run --no-sources --active pytest -k "test_dataset_determinism[train]" -s
 else
     # Otherwise, execute the provided command
     exec "$@"
