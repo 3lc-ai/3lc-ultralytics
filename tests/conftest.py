ddbf6979631022d86b7548948fade2d4a23ab601
Frederik Mellbye <frederik.mellbye@3lc.ai>
2025-07-10 15:31:41 +0200

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ def pytest_sessionstart(session):
     TMP.mkdir(parents=True, exist_ok=True)
 
     # Create default folders once (no racing)
-    import tlc  # noqa: F401, I001
+    import tlc  # noqa: F401
 
 
 def pytest_sessionfinish(session, exitstatus):
