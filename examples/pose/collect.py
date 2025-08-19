74fe0e5d0abf9cfca58ec97b714c555e263aeb80
Gudbrand Tandberg <gudbrand@3lc.ai>
2025-08-19 09:45:40 +0200

    Collect script

--- examples/pose/collect.py
+++ examples/pose/collect.py
@@ -1,8 +1,11 @@
 import tlc
-from pose_helpers import collect_pose_metrics
 
+# from pose_helpers import collect_pose_metrics
 from tlc_ultralytics import YOLO
 
 model = YOLO("yolo11n-pose.pt")
 table = tlc.Table.from_url("<TEST_DATA>/projects/Geometry/datasets/default-dataset/tables/COCO Keypoints")
-collect_pose_metrics(model, table)
+# collect_pose_metrics(model, table)
+model.collect(
+    tables={"train": table},
+)
