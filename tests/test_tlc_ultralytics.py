0e8e95f6709c6a87715a9f7160362bfb64ae4559
Frederik Mellbye <frederik.mellbye@3lc.ai>
2025-10-03 19:03:19 +0200

    Further improvement of test

--- tests/test_tlc_ultralytics.py
+++ tests/test_tlc_ultralytics.py
@@ -173,6 +173,12 @@ def test_training(task) -> None:
     settings_loaded = Settings.from_yaml(results_3lc.save_dir / "settings_3lc.yaml")
     assert settings_loaded.project_name == settings.project_name, "Project name mismatch"
 
+    run_state_file_path = results_3lc.save_dir / "run.json"
+    assert run_state_file_path.exists(), "Run state file not found"
+    with open(run_state_file_path) as f:
+        run_state = json.load(f)
+    assert run_state["run_url"] == results_3lc.run_url, "Run state mismatch"
+
     # Get 3LC run and inspect the results
     run = _get_run_from_settings(settings)
 
