d955c80bea58769a1e15291ed1a171e5986d5ba0
Gudbrand Tandberg <gudbrand@3lc.ai>
2025-10-09 10:24:46 +0200

    Use all classes

--- src/tlc_ultralytics/pose/validator.py
+++ src/tlc_ultralytics/pose/validator.py
@@ -60,7 +60,7 @@ class TLCPoseValidator(TLCValidatorMixin, PoseValidator):
 
     def _get_metrics_schemas(self) -> dict[str, tlc.Schema]:
         predicted_pose_schema = Keypoints2DSchema(
-            classes=[self.data["names"][0]],
+            classes=self.data["names"],
             num_keypoints=self.kpt_shape[0],
             points=self.data.get("points"),
             point_attributes=self.data.get("keypoint_attributes"),
