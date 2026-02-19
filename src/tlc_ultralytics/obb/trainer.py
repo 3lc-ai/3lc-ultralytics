5a119b99630fdca57da5aed3ce1922d66553a0d1
Frederik J. Mellbye <20386018+fredmell@users.noreply.github.com>
2026-02-19 12:39:17 +0100

    Simplify trainers (#53)

--- src/tlc_ultralytics/obb/trainer.py
+++ src/tlc_ultralytics/obb/trainer.py
@@ -1,31 +1,15 @@
-from copy import deepcopy
-
 from ultralytics.models.yolo.obb.train import OBBTrainer
 
 from tlc_ultralytics.constants import OBB_LABEL_COLUMN_NAME
 from tlc_ultralytics.detect.trainer import TLCDetectionTrainer
+from tlc_ultralytics.engine.trainer import TLCTrainerMixin
 from tlc_ultralytics.obb.validator import TLCOBBValidator
 
 
 class TLCOBBTrainer(OBBTrainer, TLCDetectionTrainer):
     _default_label_column_name = OBB_LABEL_COLUMN_NAME
+    _validator_class = TLCOBBValidator
+    _loss_names = ("box_loss", "seg_loss", "cls_loss", "dfl_loss")
 
-    def _process_metrics(self, metrics):
-        detection_metrics = super()._process_metrics(metrics)
-        return detection_metrics
-
-    def get_validator(self, dataloader=None):
-        self.loss_names = "box_loss", "seg_loss", "cls_loss", "dfl_loss"
-
-        if not dataloader:
-            dataloader = self.test_loader
-
-        return TLCOBBValidator(
-            dataloader,
-            save_dir=self.save_dir,
-            args=deepcopy(self.args),
-            _callbacks=self.callbacks,
-            run=self._run,
-            settings=self._settings,
-            training=True,
-        )
+    # Explicit binding to ensure TLCTrainerMixin.get_validator wins over OBBTrainer.get_validator in MRO
+    get_validator = TLCTrainerMixin.get_validator
